2596f502fc8a971ca89c234c2bab8cf97cca5c08
Oskar Zapolski <oskar.zapolski9@gmail.com>
2024-01-21 11:21:20 +0100

    mobile form styling

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -26,54 +26,62 @@ export function Form({ price, dispatch, style }) {
   return (
     <div className="background-blur" style={style}>
       <form className="pay-form">
-        <label htmlFor="fname" className="form-label">
-          First name
-        </label>
-        <input
-          type="text"
-          id="fname"
-          name="fname"
-          className="form-input"
-          placeholder="First name"
-          value={formData.firsName}
-          onChange={(e) => InputHandler(e.target.name, e.target.value)}
-        />
-        <label htmlFor="lname" className="form-label">
-          Last name
-        </label>
-        <input
-          type="text"
-          id="lname"
-          name="lname"
-          className="form-input"
-          placeholder="Last name"
-          value={formData.lastName}
-          onChange={(e) => InputHandler(e.target.name, e.target.value)}
-        />
-        <label htmlFor="mail" className="form-label">
-          Mail
-        </label>
-        <input
-          type="mail"
-          id="mail"
-          name="mail"
-          className="form-input"
-          placeholder="default@mail.com"
-          value={formData.mail}
-          onChange={(e) => InputHandler(e.target.name, e.target.value)}
-        />
-        <label htmlFor="address" className="form-label">
-          Address
-        </label>
-        <input
-          type="text"
-          id="address"
-          name="address"
-          className="form-input"
-          placeholder="Address"
-          value={formData.address}
-          onChange={(e) => InputHandler(e.target.name, e.target.value)}
-        />
+        <div className="form-flex">
+          <label htmlFor="fname" className="form-label">
+            First name
+          </label>
+          <input
+            type="text"
+            id="fname"
+            name="fname"
+            className="form-input"
+            placeholder="First name"
+            value={formData.firsName}
+            onChange={(e) => InputHandler(e.target.name, e.target.value)}
+          />
+        </div>
+        <div className="form-flex">
+          <label htmlFor="lname" className="form-label">
+            Last name
+          </label>
+          <input
+            type="text"
+            id="lname"
+            name="lname"
+            className="form-input"
+            placeholder="Last name"
+            value={formData.lastName}
+            onChange={(e) => InputHandler(e.target.name, e.target.value)}
+          />
+        </div>
+        <div className="form-flex">
+          <label htmlFor="mail" className="form-label">
+            Mail
+          </label>
+          <input
+            type="mail"
+            id="mail"
+            name="mail"
+            className="form-input"
+            placeholder="default@mail.com"
+            value={formData.mail}
+            onChange={(e) => InputHandler(e.target.name, e.target.value)}
+          />
+        </div>
+        <div className="form-flex">
+          <label htmlFor="address" className="form-label">
+            Address
+          </label>
+          <input
+            type="text"
+            id="address"
+            name="address"
+            className="form-input"
+            placeholder="Address"
+            value={formData.address}
+            onChange={(e) => InputHandler(e.target.name, e.target.value)}
+          />
+        </div>
         <div className="div-submit">
           <p>Price: {price}$</p>
           <button
